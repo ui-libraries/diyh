02f7c512f5fe5211ced939b04588d8b6ea719c39
Matthew Butler <matthewtbutler@gmail.com>
2014-10-10 11:12:38 -0500

    style simple contact for and password reset

---
+++ simple-contact-form/index/index.php
@@ -0,0 +1,96 @@
+<?php echo head(); ?>
+<style type="text/css">
+body {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #eee;
+}
+
+#primary {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #eee;
+    width: 50%;
+    margin: 0 auto;
+
+}
+
+h1 {
+    width: 50%;
+    margin: 0 auto;
+}
+
+#contact_form {
+      padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #eee;
+  width: 100%;
+    width: 50%;
+    margin: 0 auto;
+}
+
+#simple-contact {
+      padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #eee;
+  width: 100%;
+    width: 50%;
+    margin: 0 auto;
+}
+
+
+
+</style>
+<br /><br /><br /><br />
+<div id="primary">
+    <h1><?php echo html_escape(get_option('simple_contact_form_contact_page_title')); ?></h1>
+<div id="simple-contact">
+    <div id="form-instructions">
+        <?php echo get_option('simple_contact_form_contact_page_instructions'); // HTML ?>
+    </div>
+    <?php echo flash(); ?>
+    <form name="contact_form" id="contact-form"  method="post" enctype="multipart/form-data" accept-charset="utf-8">
+
+        <fieldset>
+        <div class="field">
+        <?php echo $this->formLabel('name', 'Your Name: '); ?>
+            <div class='inputs'>
+            <?php echo $this->formText('name', $name, array('class'=>'textinput')); ?>
+            </div>
+        </div>
+        
+        <div class="field">
+            <?php echo $this->formLabel('email', 'Your Email: '); ?>
+            <div class='inputs'>
+                <?php echo $this->formText('email', $email, array('class'=>'textinput'));  ?>
+            </div>
+        </div>
+        
+        <div class="field">
+          <?php echo $this->formLabel('message', 'Your Message: '); ?>
+          <div class='inputs'>
+          <?php echo $this->formTextarea('message', $message, array('class'=>'textinput', 'rows' => '10')); ?>
+          </div>
+        </div>
+        
+        </fieldset>
+
+
+        <fieldset>
+        <?php if ($captcha): ?>
+        <div class="field">
+            <?php echo $captcha; ?>
+        </div>
+        <?php endif; ?>
+
+        <div class="field">
+          <?php echo $this->formSubmit('send', 'Send Message'); ?>
+        </div>
+        
+        </fieldset>
+    </form>
+
+</div>
+
+</div>
+<?php echo foot();
--- users/forgot-password.php
+++ users/forgot-password.php
@@ -2,6 +2,40 @@
 $pageTitle = __('Forgot Password');
 echo head(array('title' => $pageTitle, 'bodyclass' => 'login'), $header);
 ?>
+<style type="text/css">
+
+body {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #eee;
+}
+
+form  {
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+  margin-bottom: 10px;
+  font-weight: normal;
+}
+
+#login-links {
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+  margin-bottom: 10px;
+  font-weight: normal;
+}
+
+h1 {
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+  margin-bottom: 10px;  
+}
+
+
+</style>
+<br /><br /><br />
 <h1><?php echo $pageTitle; ?></h1>
 <p id="login-links">
 <span id="backtologin"><?php echo link_to('users', 'login', __('Back to Log In')); ?></span>
