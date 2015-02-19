66c9ad312ecda6618ccd1488a96c516b5af95c82
Matthew Butler <matthewtbutler@gmail.com>
2015-02-19 08:52:35 -0600

    google analytics

--- common/header.php
+++ common/header.php
@@ -37,6 +37,18 @@
     <?php queue_js_file('vendor/filter-fade-portfolio'); ?>
 
     <?php echo head_js(); ?>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-783364-57', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
     
 </head>
 
